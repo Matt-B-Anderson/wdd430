3e650ccdf2aeafbc99edec34ddd74b02353c8199
Matt Anderson <manderson0182@gmail.com>
2025-11-12 15:15:54 -0500

    test

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
-import { Lusitania } from "next/font/google";
+import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const lusitania = Lusitania({
+export const lusitana = Lusitana({
 	weight: "400",
 });
