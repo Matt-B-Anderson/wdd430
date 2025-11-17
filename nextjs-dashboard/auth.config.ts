e6205243f03f021d78f2d41e700a801b41a3ab0a
Matthew Anders <manderson0182@gmail.com>
2025-11-17 13:40:23 -0500

    Updated proxy.ts to be middleware.ts since the guide is missleading

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,21 +1,16 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import type { NextAuthConfig } from "next-auth";
+
 export const authConfig = {
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
-  providers: [],
+	pages: {
+		signIn: "/login",
+	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			if (nextUrl.pathname.startsWith("/dashboard")) {
+				return !!auth?.user;
+			}
+			return true;
+		},
+	},
+	providers: [],
 } satisfies NextAuthConfig;
